eecceea5fe1c9647040ea0a403fc7cc39ce79f89
Chiaki Uehira <me@uurr.io>
2020-04-10 17:17:01 +0900

    Update

--- layouts/leave-a-message.vue
+++ layouts/leave-a-message.vue
@@ -1,41 +1,5 @@
 <template>
   <div>
-    <div class="header">
-      <div class="header-logo">
-        <img
-          src="/logo.svg"
-          alt="">
-      </div>
-      <div class="header-channel">
-        <a class="header-channel-link" href="">
-          <span class="header-channel-name">
-            PIXELGRAM<br>
-            Channel
-          </span>
-        </a>
-      </div>
-    </div>
     <nuxt />
-    <div class="footer">
-      © 2020 PIXELGRAM LLC.
-    </div>
   </div>
 </template>
-
-<style>
-.header {
-  padding: 20px;
-}
-
-.footer {
-  font-family: 'Inter', sans-serif;
-  font-weight: bold;
-  letter-spacing: 2px;
-  margin-top: 200px;
-  background-color: #000000;
-  padding: 24px 20px;
-  text-align: right;
-  color: #fff;
-  font-size: 10px;
-}
-</style>
--- pages/leave-a-message/index.vue
+++ pages/leave-a-message/index.vue
@@ -1,176 +1,208 @@
 <template>
-  <div class="container">
-    <div class="logo">
-      <img
-        src="/leave-a-message/logo.svg"
-        alt="leave-a-message">
-    </div>
-    <div class="section">
-      <p class="paragraph">
-        世界中に暗い影を落としているCOVID-19（新型コロナウイルス感染症）。
-      </p>
-      <p class="paragraph">
-        この暗闇がいつ明けるのかも分からない今、いちフリーランスのチームである
-      </p>
-      <p class="paragraph">
-        私たちPIXELGRAMに何ができるのかを考えました。
-      </p>
-      <p class="paragraph">
-        各地で様々な影響を及ぼしている中、バンドマンである私自身でいうと、
-      </p>
-      <p class="paragraph">
-        生きがいであり生活の糧であるライブが次々と中止・延期に。
-      </p>
-      <p class="paragraph">
-        また、PIXELGRAMで運営しているコワーキング兼イベントスペースのORANGE PARK（オレンジパーク）は、
-      </p>
-      <p class="paragraph">
-        感染拡大の防止のため2ヶ月間の休業。いつ再開できるかもわからない状態にいます。
-      </p>
-      <p class="paragraph">
-        一人一人の苛立ちや焦りが少しずつ溢れ出し、街やネットの雰囲気はマジで最悪。
-      </p>
-      <p class="paragraph">
-        そんな状況を換気しようと動いてみてもCOVID-19の勢いに飲み込まれ、
-      </p>
-      <p class="paragraph">
-        ただただ悪化していく状況を眺めることしかできませんでした。
-      </p>
-      <p class="paragraph">
-        とはいえ、ただ黙って受け入れらるわけもなく、自分たちに何ができて、何をすべきかを考えた結果、
-      </p>
-      <p class="paragraph">
-        ただただ悪化していく状況を眺めることしかできませんでした。
-      </p>
-      <p class="paragraph bold">
-        「今、各地で起こっていることやリアルな想いを集めて記録し、<br>共有して、未来に残す必要がある」という答えに辿り着きました。
-      </p>
-    </div>
-    <div class="section">
-      <p class="header01">
-        今、起きていることやあなたの想いを動画に残して届けてください。
-      </p>
-      <p class="paragraph">
-        買い占めは無意味だ。こんな補償を必要としている。あれはフェイクだ気をつけて。
-      </p>
-      <p class="paragraph">
-        メディアが報じていることや政府の提言のほかに、実生活ではこんな風に困っていて、
-      </p>
-      <p class="paragraph">
-        こういったことで不安な毎日を過ごしているだとか、あるいは、こんなことに希望を感じているなど、
-      </p>
-      <p class="paragraph">
-        国籍、性別、年齢、職業など関係なく、一人一人りにぞれぞれ受けた影響や考えがある。
-      </p>
-      <p class="paragraph">
-        私たちのように、今すぐ現状を大きく変える力がなくても、現状を憂い、
-      </p>
-      <p class="paragraph">
-        何かできないかと考えている人もたくさんいる。
-      </p>
-      <p class="paragraph">
-        そういった、それぞれの今ある状況やリアルな想いを、
-      </p>
-      <p class="paragraph">
-        過去に置いてけぼりにすることなく、<span class="bold">記録し、共有し、残す。</span>
-      </p>
-      <p class="paragraph">
-        これこそが私たちにできることで、すべきことではないかと思い立ち、
-      </p>
-      <p class="paragraph">
-        できる限り多くのメッセージ動画を集め、残していくことにしました。
-      </p>
-      <p class="paragraph">
-        この記録はいつの日か、再び今のような状況になってしまった時に、
-      </p>
-      <p class="paragraph">
-        事前に防げることもあれば、困っていることが解決できたり、
-      </p>
-      <p class="paragraph">
-        もしかすると命を救うための役に立つかもしれません。
-      </p>
-      <p class="paragraph">
-        あるいは、自分たちの知らない場所で起こっていることや誰かの想いを知ることで、
-      </p>
-      <p class="paragraph">
-        今まさに抱えている不安を無くせるかもしれないし、
-      </p>
-      <p class="paragraph">
-        私たちがさまよっている厳しい現状を打開できるきっかけとなるかもしれません。
-      </p>
-      <p class="paragraph">
-        なので、あなたの周りの状況や困っていること、今の想いを動画に残して届けてほしいのです。
-      </p>
-      <p class="paragraph">
-        ひとりひとりの意識と声が、希望の光になる。明日より先の未来を変えられる。そう信じています。
-      </p>
-      <p class="paragraph">
-        あなたの声を求めています。
-      </p>
-      <p class="paragraph">
-        あなたの今を教えてください。
-      </p>
-      <p class="paragraph">
-        一日でも早い事態の終息と皆様の健康と安全を願って。
-      </p>
-      <p class="paragraph bold">
-        #leave_a_message<br>「メッセージをどうぞ」
-      </p>
-      <p class="signature">
-        2020.4.5<br>
-        GIMA☆KENTA – PIXELGRAM<br>
-        発起人：PIXELGRAM
-      </p>
-    </div>
-    <div class="section">
-      <p class="header02">
-        #leave_a_message 動画の残し方
-      </p>
-      <div class="list">
-        <div class="list-item">
-          冒頭で、年月日（2020年0月0日）と撮影場所（録画した都市または地域）を言う
-        </div>
-        <div class="list-item">
-          自己紹介<br>
-          名前 ※本名でなくてもOK<br>
-          年齢 ※任意<br>
-          職業 ※任意
-        </div>
-        <div class="list-item">
-          下記への回答<br>
-          <div class="list-item number01">
-            COVID-19（新型コロナウイルス感染症）で受けた影響
-          </div>
-          <div class="list-item number02">
-            いま、困っていること
-          </div>
-          <div class="list-item number03">
-            いま何がしたいか／何をしてほしいか
-          </div>
-          <div class="list-item number04">
-            世界中の人に一言伝えられるとすれば、何を伝えたいか
-          </div>
-        </div>
+  <div class="page">
+    <div class="header">
+      <div class="header-logo">
+        <img
+          src="/logo.svg"
+          alt="">
       </div>
-    </div>
-    <div class="section">
-      <div class="button-outer">
-        <div class="button">
-          <a
-            class="button-link"
-            target="_blank"
-            href="https://m.youtube.com/channel/UCJSMWiWOe7L4HgHmXXvYZwA">
-            <span class="button-inner">
-              <span class="button-icon">
-                <img
-                  src="/leave-a-message/youtube.svg"
-                  alt="">
-              </span>
-              PIXELGRAM Channel
+      <div class="header-channel">
+        <a
+          class="header-channel-link"
+          rel="noopener"
+          target="_blank"
+          href="https://m.youtube.com/channel/UCJSMWiWOe7L4HgHmXXvYZwA">
+          <span class="header-channel-inner">
+            <span class="header-channel-icon">
+              <img
+                src="/leave-a-message/youtube.svg"
+                alt="">
             </span>
-          </a>
+            <span class="header-channel-name">
+              PIXELGRAM<br>
+              Channel
+            </span>
+          </span>
+        </a>
+      </div>
+    </div>
+    <div class="container">
+      <div class="logo">
+        <img
+          src="/leave-a-message/logo.svg"
+          alt="leave-a-message">
+      </div>
+      <div class="section">
+        <p class="paragraph">
+          世界中に暗い影を落としているCOVID-19（新型コロナウイルス感染症）。
+        </p>
+        <p class="paragraph">
+          この暗闇がいつ明けるのかも分からない今、いちフリーランスのチームである
+        </p>
+        <p class="paragraph">
+          私たちPIXELGRAMに何ができるのかを考えました。
+        </p>
+        <p class="paragraph">
+          各地で様々な影響を及ぼしている中、バンドマンである私自身でいうと、
+        </p>
+        <p class="paragraph">
+          生きがいであり生活の糧であるライブが次々と中止・延期に。
+        </p>
+        <p class="paragraph">
+          また、PIXELGRAMで運営しているコワーキング兼イベントスペースのORANGE PARK（オレンジパーク）は、
+        </p>
+        <p class="paragraph">
+          感染拡大の防止のため2ヶ月間の休業。いつ再開できるかもわからない状態にいます。
+        </p>
+        <p class="paragraph">
+          一人一人の苛立ちや焦りが少しずつ溢れ出し、街やネットの雰囲気はマジで最悪。
+        </p>
+        <p class="paragraph">
+          そんな状況を換気しようと動いてみてもCOVID-19の勢いに飲み込まれ、
+        </p>
+        <p class="paragraph">
+          ただただ悪化していく状況を眺めることしかできませんでした。
+        </p>
+        <p class="paragraph">
+          とはいえ、ただ黙って受け入れらるわけもなく、自分たちに何ができて、何をすべきかを考えた結果、
+        </p>
+        <p class="paragraph">
+          ただただ悪化していく状況を眺めることしかできませんでした。
+        </p>
+        <p class="paragraph bold">
+          「今、各地で起こっていることやリアルな想いを集めて記録し、<br>共有して、未来に残す必要がある」という答えに辿り着きました。
+        </p>
+      </div>
+      <div class="section">
+        <p class="header01">
+          今、起きていることやあなたの想いを動画に残して届けてください。
+        </p>
+        <p class="paragraph">
+          買い占めは無意味だ。こんな補償を必要としている。あれはフェイクだ気をつけて。
+        </p>
+        <p class="paragraph">
+          メディアが報じていることや政府の提言のほかに、実生活ではこんな風に困っていて、
+        </p>
+        <p class="paragraph">
+          こういったことで不安な毎日を過ごしているだとか、あるいは、こんなことに希望を感じているなど、
+        </p>
+        <p class="paragraph">
+          国籍、性別、年齢、職業など関係なく、一人一人りにぞれぞれ受けた影響や考えがある。
+        </p>
+        <p class="paragraph">
+          私たちのように、今すぐ現状を大きく変える力がなくても、現状を憂い、
+        </p>
+        <p class="paragraph">
+          何かできないかと考えている人もたくさんいる。
+        </p>
+        <p class="paragraph">
+          そういった、それぞれの今ある状況やリアルな想いを、
+        </p>
+        <p class="paragraph">
+          過去に置いてけぼりにすることなく、<span class="bold">記録し、共有し、残す。</span>
+        </p>
+        <p class="paragraph">
+          これこそが私たちにできることで、すべきことではないかと思い立ち、
+        </p>
+        <p class="paragraph">
+          できる限り多くのメッセージ動画を集め、残していくことにしました。
+        </p>
+        <p class="paragraph">
+          この記録はいつの日か、再び今のような状況になってしまった時に、
+        </p>
+        <p class="paragraph">
+          事前に防げることもあれば、困っていることが解決できたり、
+        </p>
+        <p class="paragraph">
+          もしかすると命を救うための役に立つかもしれません。
+        </p>
+        <p class="paragraph">
+          あるいは、自分たちの知らない場所で起こっていることや誰かの想いを知ることで、
+        </p>
+        <p class="paragraph">
+          今まさに抱えている不安を無くせるかもしれないし、
+        </p>
+        <p class="paragraph">
+          私たちがさまよっている厳しい現状を打開できるきっかけとなるかもしれません。
+        </p>
+        <p class="paragraph">
+          なので、あなたの周りの状況や困っていること、今の想いを動画に残して届けてほしいのです。
+        </p>
+        <p class="paragraph">
+          ひとりひとりの意識と声が、希望の光になる。明日より先の未来を変えられる。そう信じています。
+        </p>
+        <p class="paragraph">
+          あなたの声を求めています。
+        </p>
+        <p class="paragraph">
+          あなたの今を教えてください。
+        </p>
+        <p class="paragraph">
+          一日でも早い事態の終息と皆様の健康と安全を願って。
+        </p>
+        <p class="paragraph bold">
+          #leave_a_message<br>「メッセージをどうぞ」
+        </p>
+        <p class="signature">
+          2020.4.5<br>
+          GIMA☆KENTA – PIXELGRAM<br>
+          発起人：PIXELGRAM
+        </p>
+      </div>
+      <div class="section">
+        <p class="header02">
+          #leave_a_message 動画の残し方
+        </p>
+        <div class="list">
+          <div class="list-item">
+            冒頭で、年月日（2020年0月0日）と撮影場所（録画した都市または地域）を言う
+          </div>
+          <div class="list-item">
+            自己紹介<br>
+            名前 ※本名でなくてもOK<br>
+            年齢 ※任意<br>
+            職業 ※任意
+          </div>
+          <div class="list-item">
+            下記への回答<br>
+            <div class="list-item number01">
+              COVID-19（新型コロナウイルス感染症）で受けた影響
+            </div>
+            <div class="list-item number02">
+              いま、困っていること
+            </div>
+            <div class="list-item number03">
+              いま何がしたいか／何をしてほしいか
+            </div>
+            <div class="list-item number04">
+              世界中の人に一言伝えられるとすれば、何を伝えたいか
+            </div>
+          </div>
         </div>
       </div>
+      <div class="section">
+        <div class="button-outer">
+          <div class="button">
+            <a
+              class="button-link"
+              rel="noopener"
+              target="_blank"
+              href="https://m.youtube.com/channel/UCJSMWiWOe7L4HgHmXXvYZwA">
+              <span class="button-inner">
+                <span class="button-icon">
+                  <img
+                    src="/leave-a-message/youtube.svg"
+                    alt="">
+                </span>
+                PIXELGRAM Channel
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      © 2020 PIXELGRAM LLC.
     </div>
   </div>
 </template>
@@ -182,6 +214,51 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 80px;
+    padding: 8px 20px;
+    @media screen and (max-width: 768px) {
+      padding: 8px 12px;
+      margin-bottom: 126px;
+    }
+    &-channel {
+      &-inner {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      &-icon {
+        margin-right: 8px;
+        @media screen and (max-width: 768px) {
+          margin-right: 0;
+          width: 48px;
+        }
+      }
+      &-name {
+        @media screen and (max-width: 768px) {
+          display: none;
+        }
+      }
+      &-link {
+        display: inline-block;
+        line-height: 1.2;
+        border-radius: 6px;
+        letter-spacing: 2px;
+        padding: 12px 16px;
+        background-color: #FF6400;
+        font-weight: 900;
+        font-size: 16px;
+        text-decoration: none;
+        color: #fff;
+        @media screen and (max-width: 768px) {
+          padding: 16px 10px;
+        }
+      }
+    }
+  }
   .container {
     max-width: 998px;
     width: 100%;
